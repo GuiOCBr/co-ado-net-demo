d102f62185ebbec13d879573c920c819dbdf6184
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2024-05-30 18:15:31 -0300

    change: remove

--- DataAcessDemo.API/Repositories/UserRepository.cs
+++ DataAcessDemo.API/Repositories/UserRepository.cs
@@ -7,26 +7,26 @@ public class UserRepository : IUserRepository
 
 	public List<User> Get()
 	{
-		throw new NotImplementedException();
+		
 	}
 
 	public User Get(int id)
 	{
-		throw new NotImplementedException();
+		
 	}
 
 	public void Insert(User user)
 	{
-		throw new NotImplementedException();
+		
 	}
 
 	public void Update(User user)
 	{
-		throw new NotImplementedException();
+		
 	}
 
 	public void Delete(int id)
 	{
-		throw new NotImplementedException();
+		
 	}
 }
